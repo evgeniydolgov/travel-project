f501dba9d1eb7396dbfa6ae7ae185457eb875b54
evgeniydolgov <amyr214519@gmail.com>
2022-08-11 17:35:45 +0300

    init: start momentum task

---
+++ momentum/js/playList.js
@@ -0,0 +1,28 @@
+const playList = [
+    {      
+        title: 'Peer Gynt',
+        src: '../assets/sounds/Peer Gynt.mp3',
+        duration: '02:18'
+    },  
+    {      
+        title: 'Aqua Caelestis',
+        src: '../assets/sounds/Aqua Caelestis.mp3',
+        duration: '00:39'
+    },  
+    {      
+        title: 'Ennio Morricone',
+        src: '../assets/sounds/Ennio Morricone.mp3',
+        duration: '01:37'
+    },  
+    {      
+        title: 'River Flows In You',
+        src: '../assets/sounds/River Flows In You.mp3',
+        duration: '01:37'
+    },
+    {      
+        title: 'Summer Wind',
+        src: '../assets/sounds/Summer Wind.mp3',
+        duration: '01:50'
+    }
+  ]
+  export default playList;
